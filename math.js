061d5d50bf62334885fb3e206ba12dc310eaa579
Alastor <cryptosenor@protonmail.com>
2022-12-03 13:42:21 -0500

    sub

---
+++ math.js
@@ -0,0 +1,7 @@
+const add = (a,b) =>a +b;
+const Restar = (a,b) =>a *b; 
+const Multiplicar = (a,b) =>a *b; 
+const Dividir = (a,b) =>a /b;  
+
+
+module.exports = { add, Restar, Multiplicar, Dividir}
--- server.js
+++ server.js
@@ -7,9 +7,12 @@ console.log(globalThis);
 
 const os = require('os');
 
+
+
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
 
 console.log(__dirname);
 console.log(__filename);
+
