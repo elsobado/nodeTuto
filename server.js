a315ddb1ea2824eddc4635c999a8c22f9dfc702e
Alastor <cryptosenor@protonmail.com>
2022-12-03 01:56:45 -0500

    rama uno

---
+++ server.js
@@ -0,0 +1,15 @@
+// Tienes que saber algo de :
+// HTML,CSS, y algo de JS
+// POsible algo de expe en framwework como RESCTm o sim ilar
+
+console.log("hola  cero t");
+console.log(globalThis);
+
+const os = require('os');
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
